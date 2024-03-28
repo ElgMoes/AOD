ab45b036b6ffed830c107cecfd6ecc04455c2e75
Jelle Hoek <jellewhoek@gmail.com>
2024-03-28 13:44:23 +0100

    Included ipynb

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ lat: list = [drifter[i][1] for i in range(N)]
 time: list = [np.datetime64(drifter[i][0]) for i in range(N)]
 
 # Slice maken om stukken van de lijsten te selecteren
-begin: int = 1500
-eind: int = 2500
+begin: int = 3694
+eind: int = 3959
 
 I: slice = slice(begin, eind)
 
@@ -32,23 +32,35 @@ tijd_verschil = tijd_eind - tijd_begin
 projection: object = cartopy.crs.PlateCarree()
 
 # initialisatie van de figuur
-fig: object = plt.figure(figsize=(12, 5))
-ax: object = fig.add_subplot(1, 1, 1, projection=projection)
+fig_drifter: object = plt.figure(figsize=(12, 5))
+fig_ssh: object = plt.figure(figsize=(12, 10))
+ax_drifter: object = fig_drifter.add_subplot(1, 1, 1, projection=projection)
 
 # plot de kustlijnen en maak het land beige
-ax.coastlines(resolution='50m')
-ax.add_feature(cartopy.feature.LAND)
+ax_drifter.coastlines(resolution='50m')
+ax_drifter.add_feature(cartopy.feature.LAND)
 
 # plot het traject, let op dat we een transformatie moeten gebruiken naar cartopy.crs.PlateCarree(),
 # ook als je hierboven de projectie hebt veranderd
-ax.plot(lon[I], lat[I], transform=cartopy.crs.PlateCarree())
+ax_drifter.plot(lon[I], lat[I], transform=cartopy.crs.PlateCarree())
 
 # de code hieronder plot lichtgrijze lijnen voor het longitude/latitude grid en zorgt voor goede formatiing van de labels
-gl: object = ax.gridlines(crs=cartopy.crs.PlateCarree(), draw_labels=True, linewidth=0.5,
+gl: object = ax_drifter.gridlines(crs=cartopy.crs.PlateCarree(), draw_labels=True, linewidth=0.5,
               color='gray', alpha=0.5, linestyle='--')
 gl.top_labels = False
 gl.right_labels = False
 gl.xformatter = cartopy.mpl.gridliner.LONGITUDE_FORMATTER
 gl.yformatter = cartopy.mpl.gridliner.LATITUDE_FORMATTER
+
+# initialisatie figuur
+#fig_ssh: object = plt.figure(figsize=(12,5))
+ax_ssh: object = fig_ssh.add_subplot(1, 1, 1)
+
+# openen van data en selecteren SSH
+SSH = xr.open_dataset('global-analysis-forecast-phy-001-024_SSH.nc').zos.mean('time')
+SSH.plot(ax=ax_ssh, cbar_kwargs=dict(label='SSH [meter]'))
+
+# Plot ook het relevante gedeelte van het drifter-traject op de kaart
+ax_ssh.plot(lon[I], lat[I], 'C01', linewidth=4)
 
 plt.show()
